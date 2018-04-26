63355422bb238f2b6a69fa397bb50865d38a1333
Breyanna Vass <breyannavass@gmail.com>
2018-04-26 18:05:29 -0400

    Changes to views and controllers

--- capture_media/app/controllers/users/registrations_controller.rb
+++ capture_media/app/controllers/users/registrations_controller.rb
@@ -16,12 +16,11 @@ class Users::RegistrationsController < Devise::RegistrationsController
   def create
     super
     @user = User.new(user_params)
-    binding.pry
     if @user.save
       session[:user_id] = @user.id
       redirect_to user_path(@user)
     else
-      redirect_to :new
+      return :new
     end
   end
 
@@ -87,7 +86,7 @@ class Users::RegistrationsController < Devise::RegistrationsController
   private
 
   def user_params
-    params.require(:user).permit(:name, :password, :happiness, :admin)
+    params.require(:user).permit(:first_name, :last_name, :email, :password_confirmation)
   end
 
   def sign_up_params
