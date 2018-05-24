a7444d24e95d39fb84353038a1a92679f49d437a
Breyanna Vass <breyannavass@gmail.com>
2018-05-23 22:13:47 -0400

    Possible user submittable attribute

--- capture_media/app/models/appointment.rb
+++ capture_media/app/models/appointment.rb
@@ -9,4 +9,11 @@ class Appointment < ApplicationRecord
     validates :date, presence: true
 
     scope :recent, ->{ order("created_at DESC") }
+
+    def pictures_attributes=(pictures_attributes)
+      pictures_attributes.values.each do |picture_attribute|
+        picture = Picture.find_or_create_by(picture_attribute)
+        pictures << picture if picture.persisted?
+      end   
+    end
 end
