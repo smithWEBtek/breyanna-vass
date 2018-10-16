7c8163972eac04f48b7e5736dc56e66c47661419
Breyanna Vass <breyannavass@gmail.com>
2018-10-16 16:19:42 -0400

    Adding a prototype

--- capture_media/app/assets/javascripts/appointments.js
+++ capture_media/app/assets/javascripts/appointments.js
@@ -1,3 +1,15 @@
+function Appointment(name, date, quantity) {
+    this.name = name;
+    this.date = date;
+    this.quantity = quantity;
+}
+
+Appointment.prototype.youScheduled = function() {
+    console.log(`Hello, ${this.name}, your appointment is ${this.date}.`);
+};
+
+
+
 $(function(){
     $("a.load_package").on("click", function(e) { 
         $.ajax({
