eb275430a660bc5142c60c3dbfa1ec264e044b88
Breyanna Vass <breyannavass@gmail.com>
2018-10-16 16:53:53 -0400

    Using JSON to create an appointment

--- capture_media/app/assets/javascripts/appointments.js
+++ capture_media/app/assets/javascripts/appointments.js
@@ -10,6 +10,28 @@ Appointment.prototype.youScheduled = function() {
     console.log(`Hello, ${this.name}, your appointment is ${this.date}.`);
 };
 
+// $(function(){
+//     $(form_for).on("submit", function(e){
+//         var $form = $(this);
+//         var action = $form.attr("action");
+//         var params = $form.serialize()
+
+//         $.ajax({
+//             url: action,
+//             data: params,
+//             dataType: "json",
+//             method: "POST"
+//         }).success(function(json){
+//             var appointment = new Appointment(json);
+//             var appointmentLi = appointment.youScheduled()
+//             $("").append(appointmentLi)
+//         })
+//         .error(function(response){
+//             console.log("You broke it?", response)
+//         })
+//         e.preventDefault();
+//     })
+// })
 
 
 $(function(){
