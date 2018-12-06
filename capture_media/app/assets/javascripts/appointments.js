bf786cede66ab61552f198778848689e382592f7
Breyanna Vass <breyannavass@gmail.com>
2018-12-06 14:59:27 -0500

    Moving formatting around

--- capture_media/app/assets/javascripts/appointments.js
+++ capture_media/app/assets/javascripts/appointments.js
@@ -1,89 +1,93 @@
-function Appointment(attributes) {
-    this.name = attributes.name;
-    this.date = attributes.date;
-    this.quantity = attributes.quantity;
-    this.package_id = attributes.package_id;
-}
+
+$(document).ready(function () {
+    listenMyAppointments();
+});
+
 
 function Package(attributes) {
     this.name = attributes.name;
     this.description = attributes.description;
 }
 
-Appointment.prototype;
 
-Appointment.prototype.youScheduled = function() {
-    return `Hello ${this.name}, your appointment is ${this.date}`;
-};
+$(function () {
 
-$(function(){
-     $("form#new_appointment").on("submit", function(e){
-         var $form = $(this);
-         var action = $form.attr("action");
-         var params = $form.serialize()
+    $("form#new_appointment").on("submit", function (e) {
+        debugger
+        var $form = $(this);
+        var action = $form.attr("action");
+        var params = $form.serialize()
 
-         $.ajax({
-             url: action,
-             data: params,
-             dataType: "json",
-             method: "POST"
-         }).success(function(json){
+        $.ajax({
+            url: action,
+            data: params,
+            dataType: "json",
+            method: "POST"
+        }).success(function (json) {
             $("div.appointments").val(" ")
             var appointment = new Appointment(json);
             var appointmentLi = appointment.youScheduled()
             document.getElementById("scheduled").innerHTML = appointmentLi
-         })
-         .error(function(response){
-             console.log("You broke it?", response)
-         })
+        })
+            .error(function (response) {
+                console.log("You broke it?", response)
+            })
         e.preventDefault();
-     })
+        listenMyAppointments();
+    })
 })
 
-$(document ).ready(function(){
-    $("a#appointments").click(function(e){
+function listenMyAppointments() {
+    $("a#appointments").click(function (e) {
         e.preventDefault();
-        //debugger
         fetchAppointments()
     })
-    function fetchAppointments(){
-        fetch('/users/1/appointments.json')
-            .then(response => response.json())
+}
+
+function fetchAppointments() {
+    fetch('/users/1/appointments.json')
+        .then(response => response.json()
             .then(data => createAppointments(data))
+        )
+}
+
+function createAppointments(data) {
+    data.forEach((appt) => {
+        let appointment = new Appointment(appt)
+        let appointmentHTML = appointment.formatDateHtml()
+        $("div#client").append(appointmentHTML + "<br>")
+        console.log("appoinment.formatAdmiredHtml()")
+        //document.getElementById("client").innerHTML += appointmentHTML + "<br>";
+    })
+}
+
+class Appointment {
+    constructor(attributes) {
+        this.name = attributes.name;
+        this.date = attributes.date;
+        this.quantity = attributes.quantity;
+        this.package_id = attributes.package_id;
     }
-    function createAppointments(data){
-        data.forEach(appt => {
-            //debugger
-            let appointment = new Appointment(appt.name, appt.date, appt.quantity, appt.package_id)
-            let appointmentHTML = appointment.formatDateHtml()
-            //debugger
-            $("div#client").append(appointmentHTML + "<br>")
-            console.log("appoinment.formatAdmiredHtml()")
-            //document.getElementById("client").innerHTML += appointmentHTML + "<br>";
-        })
-    }
-    class Appointment{
-        constructor(name, date, quantity, package_id){
-            this.name = name;
-            this.date = date;
-            this.quantity = quantity;
-            this.package_id = package_id
-        }
-    }
-    Appointment.prototype.formatDateHtml = function() {
-        // use a loop to get all of the information to every appointment
-        return `Name: ${this.name}   
+}
+
+Appointment.prototype.youScheduled = function () {
+    return `Hello ${this.name}, your appointment is ${this.date}`;
+};
+
+Appointment.prototype.formatDateHtml = function () {
+    // use a loop to get all of the information to every appointment
+    return `Name: ${this.name}   
         Date: ${this.date} 
         Quantity: ${this.quantity}   
         Package: ${this.package_id}`
-    }
-});
+}
 
- $(document).ready(function(){
-    $("a.load_package").click(function(e){
+
+$(document).ready(function () {
+    $("a.load_package").click(function (e) {
         e.preventDefault();
         const x = document.getElementById("package");
-        x.addEventListener("click", function(event){
+        x.addEventListener("click", function (event) {
             fetchPopularPackage()
         });
         function fetchPopularPackage() {
@@ -102,10 +106,10 @@ $(document ).ready(function(){
                 this.description = description;
             }
         }
-        Package.prototype.formatAdmiredHtml = function(){
+        Package.prototype.formatAdmiredHtml = function () {
             return `${this.name}  ${this.description}`
-         }
+        }
     });
-      
+
 })
-    
+
